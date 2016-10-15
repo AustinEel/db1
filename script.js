cd72f4c343ef7ae88ffd81457523b12c29333021
Austin Lee <ucaustinlee@g.ucla.edu>
2016-10-15 12:40:08 -0700

    sped up load times

--- script.js
+++ script.js
@@ -1,30 +1,14 @@
-$(document).ready(function() {
-$('a[href*=#]').each(function() {
-if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-&amp;amp;&amp;amp; location.hostname == this.hostname
-&amp;amp;&amp;amp; this.hash.replace(/#/,'') ) {
-var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
-var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-if ($target) {
-var targetOffset = $target.offset().top;
-$(this).click(function() {
-$("#nav li a").removeClass("active");
-$(this).addClass('active');
-$('a[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-      return false;
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
     }
-  }
-});
-return false;
-});
-}
-}
-});
+  });
 });
